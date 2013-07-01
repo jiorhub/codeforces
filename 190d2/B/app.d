347232caa6910938407d48fe5ad78a6a683ea5a7
Jior <jior.mail@gmail.com>
2013-07-01 14:08:52 +0400

    complete 190d2 B

--- 190d2/B/app.d
+++ 190d2/B/app.d
@@ -2,7 +2,7 @@ import std.stdio: readln, writeln;
 import std.string: strip;
 import std.array: split;
 import std.conv: to;
-import std.algorithm: min;
+import std.algorithm: min, count;
 
 /**
  * User: Jior
@@ -12,19 +12,29 @@ import std.algorithm: min;
 
 void main() {
     auto rgb = strip(readln()).split().to!(int[]);
+    int result = 0;
+   	foreach(ref color; rgb) {   		
+   		if(color) {
+   			if (color % 3) {
+   				result += color / 3;
+   				color %= 3;	
+			} else {
+				result += color / 3 - 1;
+				color = 3;
+			}
+   		}
+   	}
+ 	
+ 	switch(rgb.count(3)) {
+ 		case 2: 
+ 			result += 2;
+ 			break;
+ 		case 3:
+ 			result += 3;
+ 			break;
+ 		default:
+ 			result += min(rgb[0], rgb[1], rgb[2]);	
+ 	}
 
-    auto result = 0;
-
-    result += rgb[0] / 3;
-    rgb[0]  = rgb[0] % 3;
-
-    result += rgb[1] / 3;
-    rgb[1]  = rgb[1] % 3;
-
-    result += rgb[2] / 3;
-    rgb[2]  = rgb[2] % 3;
-
-    result += min(rgb[0], rgb[1], rgb[2]);
-    
-    writeln(result);
+   	writeln(result);
 }
